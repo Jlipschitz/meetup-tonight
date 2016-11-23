3343fd827708edfb2c2dab684dd727b594b66c58
allpwrfulroot <akunz@alum.mit.edu>
2016-11-23 10:15:34 -0500

    Live API request, with responses displayed

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const request = require('../../client/utils/request');
+// const request = require('../../client/utils/request').default;
+const axios = require('axios');
+
 // event model
 const Event = mongoose.model('Event');
 
@@ -7,9 +9,15 @@ const Event = mongoose.model('Event');
 export default router => {
 
   // Get initial app data
-  router.get('/events/search', function(req, res, next) {
-    request.get('https://api.meetup.com/find/events?key=4fb2575d64774911197925566e13f&lat=40.67&lon=-73.98')
-    .then(responseIhope => res.json(responseIhope))
+  router.get('/events/search/', function(req, res, next) {
+    axios.get('https://api.meetup.com/find/events', {
+      params: {
+        key: '4fb2575d64774911197925566e13f',
+        lat: 40.677,
+        lon: -73.982
+      }
+    })
+    .then(responseIhope => res.json(responseIhope.data))
     .catch(errors => console.log(errors))
   });
 
